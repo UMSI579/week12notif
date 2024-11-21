42a54c40e960ced415965cffcdbc2b4925b688d5
Mullins, Benjamin <benm@umich.edu>
2024-11-20 20:58:22 -0500

    picker

--- App.js
+++ App.js
@@ -1,11 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, } from 'react-native';
+import * as Notifications from 'expo-notifications';
 
 export default function App() {
+  const [ hasPermission, setHasPermission ] = useState(false);
+  const [ date, setDate ] = useState(new Date());
+  const [ inputText, setInputText ] = useState('');
+
+
+  useEffect(() => {
+    async function getPermissions(){
+      const { status } = await Notifications.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    }
+    getPermissions();
+    Notifications.cancelAllScheduledNotificationsAsync();
+  }, []);
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>Notification permissions not granted.</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <DateTimePicker
+        value={date}
+        mode="date"
+        onChange={(evt, newDate) => {
+          // update Y/M/D, keep H/M
+          setDate(new Date(newDate.getFullYear(),
+            newDate.getMonth(),
+            newDate.getDate(),
+            date.getHours(),
+            date.getMinutes()));
+        }}
+      />
+      <DateTimePicker
+        value={date}
+        mode="time"
+        onChange={(evt, newDate) => {
+          // keep Y/M/D, update H/M
+          setDate(new Date(date.getFullYear(),
+            date.getMonth(),
+            date.getDate(),
+            newDate.getHours(),
+            newDate.getMinutes()));
+        }}
+      />
+
+      <TextInput
+        style={{
+          width: '80%',
+          borderBottomWidth: 1,
+          borderBottomColor: 'gray',
+          padding: '3%'
+        }}
+        value={inputText}
+        onChangeText={(text)=>{setInputText(text)}}
+        placeholder="Remind me..."
+      />
+      <TouchableOpacity
+        onPress={async ()=>{
+          await Notifications.scheduleNotificationAsync({
+            content: {
+              title: "week 12 notify",
+              body: inputText,
+            },
+            trigger: {
+              type: Notifications.SchedulableTriggerInputTypes.DATE,
+              date: date,
+            }
+          })
+        }}
+      >
+        <Text>Schedule Notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
